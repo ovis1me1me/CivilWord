23387abbfc101e2cff282c7f3be4bd0efcffe231
ovis1me1me <ovis1me1me@gmail.com>
2025-04-05 18:47:53 +0900

    llm 응답 더미 ver

--- app/services/llm_service.py
+++ app/services/llm_service.py
@@ -1,5 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv() # .env파일 로드
+
 # LLM과 연결될 핵심 서비스 로직
 # 임시로 문자열 반환
 def generate_response(prompt: str) -> str:
-	return f"[임시 응답] '{prompt}'에 대한 답변입니다."
+    api_key = os.getenv("LLM_API_KEY", "키 없음")   # .env에 값 없으면 키 없음 출력
+    return f"[임시 응답] '{prompt}'에 대한 답변입니다.(APO KEY: {api_key})"
 
