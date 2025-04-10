138836ea1a92c21bd9147921d7bb29562b5f80bc
ovis1me1me <ovis1me1me@gmail.com>
2025-04-10 16:00:04 +0900

    아이디 유니크로 수정

--- app/models/user_info.py
+++ app/models/user_info.py
@@ -4,7 +4,7 @@ class UserInfo(Base):
     __tablename__ = "user_info"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(String, index=True)    #더미 사용자 id(로그인 이후 대체)
+    user_id = Column(String, index=True, unique=True)    #더미 사용자 id(로그인 이후 대체)
     name = Column(String)
     department = Column(String)
     contact = Column(String)
